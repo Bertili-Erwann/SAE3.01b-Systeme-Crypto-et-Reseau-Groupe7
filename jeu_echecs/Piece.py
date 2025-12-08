124b750dc40c561a0d2bd38d4de901a14196948c
Bertili-Erwann <erwann.bertili@etu.univ-orleans.fr>
2025-12-08 16:54:35 +0100

    Creation d'une piece

--- jeu_echecs/Piece.py
+++ jeu_echecs/Piece.py
@@ -1,22 +1,23 @@
+from abc import abstractmethod
+from typing import List, Tuple
 
 import Plateau
 
+
 class Piece(object):
+    def __init__(self, plateau: Plateau.Plateau, couleur: str, symbole: str, coordonnee: Tuple[int, int]):
+        self.couleur = couleur
+        self.symbole = symbole
+        self.coordonnee = coordonnee
+        self.plateau = plateau
+
+    def getCaseActu(self) -> Tuple[int, int]:
+        return self.coordonnee
+        
     @abstractmethod
-    def avancer(target):
-        pass
-        
-    def getCaseActu():
+    def avancer(self, target: Tuple[int, int]) -> None:
         pass
         
     @abstractmethod
-    def getTargets():
+    def getTargets(self) -> List[Tuple[int, int]]:
         pass
-        
-    def __init__(self):
-        self. = None
-        self. = None
-        self.couleur = None
-        self.symbole = None
-        pass
-    
