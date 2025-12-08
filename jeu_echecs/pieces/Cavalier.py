d39c14e75bb1a6dd6acd6747192632dee5f5260b
Arsamerzoev Magomed <o22401211@or-iut-i003-f14.iut45.univ-orleans.fr>
2025-12-08 18:18:30 +0100

    Refactoriser la structure du jeu d'échecs : supprimer les classes inutilisées et implémenter la logique de base du jeu dans les classes Game et Player.

--- jeu_echecs/pieces/Cavalier.py
+++
@@ -1,8 +0,0 @@
-
-import Piece
-
-class Cavalier(Piece.Piece):
-    def __init__(self):
-        super(Cavalier, self).__init__()
-        pass
-    
--- jeu_echecs/pieces/Pion.py
+++
@@ -1,8 +0,0 @@
-
-import Piece
-
-class Pion(Piece.Piece):
-    def __init__(self):
-        super(Pion, self).__init__()
-        pass
-    
