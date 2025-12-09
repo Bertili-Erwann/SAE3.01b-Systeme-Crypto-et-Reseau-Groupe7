40e6da2963621fc8606618292edf5f5aa1c2a49f
Magomed <m4g0o0@proton.me>
2025-12-09 19:25:14 +0100

    Refactoriser la méthode lance_partie : simplification de la logique de tour des joueurs et ajout de la gestion des scores.

--- jeu_echecs/Jeu.py
+++ jeu_echecs/Jeu.py
@@ -10,38 +10,76 @@ class Jeu():
     def affiche_plateau(self):
         print(self.plateau)
 
-    def lance_partie(self, jB, jN):
-        while not self.plateau.is_checkmate(
-        ) and not self.plateau.is_stalemate():
-            # Tour du blanc
-            move_blanc = chess.Move(
-                chess.parse_square("a2"), chess.parse_square("a8")
-            )  # je fais volontairement un move impossible pour entrer dans la boucle
-            while move_blanc not in self.plateau.legal_moves:
-                try:
-                    
-                    input_blanc = jB.rentre_case(self.plateau)
-                    move_blanc = chess.Move(chess.parse_square(input_blanc[0]),
-                                        chess.parse_square(input_blanc[1]))
-                except ValueError:
-                    print("Mauvais format")
-                
-            self.plateau.push(move_blanc)
-
-            # Tour du noir
-            move_noir = chess.Move(chess.parse_square("a2"),
-                                   chess.parse_square("a8"))
-            while move_noir not in self.plateau.legal_moves:
-                input_noir = jN.rentre_case(self.plateau)
-                move_noir = chess.Move(chess.parse_square(input_noir[0]),
-                                       chess.parse_square(input_noir[1]))
-            self.plateau.push(move_noir)
-
-
+    def lance_partie(self, jB, jN) -> None:
+        while not self.plateau.is_checkmate() and not self.plateau.is_stalemate():
+            for j in [jB,jN]:
+                move = chess.Move(chess.parse_square("a2"), chess.parse_square("a8"))  # je fais volontairement un move impossible pour entrer dans la boucle
+                while move not in self.plateau.legal_moves:
+                    try:
+                        input = j.rentre_case(self.plateau)
+                        move = chess.Move(chess.parse_square(input[0]),chess.parse_square(input[1]))
+                    except ValueError:
+                        print("Mauvais format")
+                self.plateau.push(move)
+                if j == jB and (self.plateau.is_checkmate() or self.plateau.is_stalemate()):
+                    break
+        #Le resultat du match 
+        resultat = self.plateau.outcome()
+        match resultat.termination  :
+            case chess.Termination.CHECKMATE: 
+                if resultat.winner:
+                    print("Vainqueur : Blanc par mat !")
+                    jB.inc_score()
+                    print(f"{jB.get_score()} - {jN.get_score()}")
+                else:
+                    print(f"Vainqueur : Noir par mat !")
+                    jN.inc_score()
+                    print(f"{jN.get_score()} - {jB.get_score()}")
+            case chess.Termination.STALEMATE: 
+                print("Match nulle !")
+                print(f"{jB.get_score()} - {jN.get_score()}")
+        self.revanche(jB,jN)            
+        
+    def revanche(self,jB,jN) -> None:
+        if(jB.revanche() and jN.revanche()):
+            self.lance_partie(jN,jB) # je fais expres de les inverser
+        else:
+            print(f"La partie s'achève avec un score de {jN.get_score()} - {jB.get_score()}")
+            
 class Joueur():
 
     def __init__(self, couleur):
         self.couleur = couleur
+        self.score = 0
+        
+    def inc_score(self) -> None :
+        """
+        Incrémente le score de 1         
+        """
+        self.score+=1
+    
+    def get_score(self) -> int:
+        return self.score
+
+    def revanche(self) -> bool:
+        """
+        Demande au joueur si il veux prendre sa revanche 
+        returns : True si il prend sa revanche, False sinon
+        """
+        while True:
+            demande = input("Voulez vous prendre votre revanche ? (O)ui/(N)on ")
+            match demande.lower():
+                case "o":
+                    continue
+                case "oui": 
+                    self.couleur = "blanc" if self.couleur == "noir" else self.couleur = "noir"
+                    return True
+                case "n":
+                    continue
+                case "non": 
+                    return False
+                case _default:
+                    print(f"{demande.lower()} n'est pas une reponse attendu")
 
     def rentre_case(self, plateau: Jeu) -> str:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -57,3 +95,8 @@ if __name__ == "__main__":
     blanc = Joueur("blanc")
     noir = Joueur("noir")
     j.lance_partie(blanc, noir)
+# f2 f3 
+# e7 e6       
+# g2 g4 
+# d8 h4 
+# la combinaison la plus rapide pour mat
