f37f3dda4dd5e513b23c1bfc2a13aec28a349a6e
Arsamerzoev Magomed <o22401211@or-iut-i207-f20.iut45.univ-orleans.fr>
2025-12-09 13:48:06 +0100

    Amélioration de la logique de jeu : ajout de la gestion des mouvements des joueurs et mise à jour de l'affichage des instructions.

--- jeu_echecs/Jeu.py
+++ jeu_echecs/Jeu.py
@@ -13,10 +13,24 @@ class Jeu():
     def lance_partie(self, jB, jN):
         while not self.plateau.is_checkmate(
         ) and not self.plateau.is_stalemate():
-            jeu_blanc = jB.rentre_case(self.plateau)
-            self.plateau.to_square()    
-            jeu_noir = jN.rentre_case(self.plateau)
-            
+            # Tour du blanc
+            move_blanc = chess.Move(
+                chess.parse_square("a2"), chess.parse_square("a8")
+            )  # je fais volontairement un move impossible pour entrer dans la boucle
+            while move_blanc not in self.plateau.legal_moves:
+                input_blanc = jB.rentre_case(self.plateau)
+                move_blanc = chess.Move(chess.parse_square(input_blanc[0]),
+                                        chess.parse_square(input_blanc[1]))
+            self.plateau.push(move_blanc)
+
+            # Tour du noir
+            move_noir = chess.Move(chess.parse_square("a2"),
+                                   chess.parse_square("a8"))
+            while move_noir not in self.plateau.legal_moves:
+                input_noir = jN.rentre_case(self.plateau)
+                move_noir = chess.Move(chess.parse_square(input_noir[0]),
+                                       chess.parse_square(input_noir[1]))
+            self.plateau.push(move_noir)
 
 
 class Joueur():
@@ -26,15 +40,15 @@ class Joueur():
 
     def rentre_case(self, plateau: Jeu) -> str:
         os.system('cls' if os.name == 'nt' else 'clear')
+        print(plateau)
         res = input(
-            f"{plateau} \nTOUR:{self.couleur}, rentrez une case parmis les cases suivante (lettre + coordonnée) \n"
+            f"\nTOUR:{self.couleur}, Entrez la coordonnée ACTUELLE de la pièce et la NOUVELLE, (ex : a2 a3) \n"
         )
-        return res
+        return res.split(" ")
 
 
 if __name__ == "__main__":
     j = Jeu()
-    j.affiche_plateau()
     blanc = Joueur("blanc")
     noir = Joueur("noir")
     j.lance_partie(blanc, noir)
